b7495c68e3917bdc70651c9cf88668ea21321d63
Nogeese <leon8326nogeese@outlook.com>
2025-03-15 09:48:07 +0000

    Update flush.sh

--- scripts/flush.sh
+++ scripts/flush.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sudo bash
 sudo mkdir -p /etc/nogeese/flush/data /etc/nogeese/flush/record
 cd /etc/nogeese/flush
-sudo touch records/0
+sudo touch record/0
 sudo echo "SYS:ARCH:$arch" >> records/0
 sudo touch data/sh
 sudo echo "SYS:SH:bash" >> data/sh
